19ce27a027c7922603b10e33c65f48ddff3bec0e
Arun verma <arun.verma_ccv18@gla.ac.in>
2020-02-24 22:16:57 +0530

    creating PERSON CLass

--- src/Person.java
+++ src/Person.java
@@ -19,4 +19,49 @@ public class Person {
     public String getLast() {
         return last;
     }
+    public void setLast(String last) {
+        this.last = last;
+    }
+
+    public String getEmai() {
+        return emai;
+    }
+
+    public void setEmai(String emai) {
+        this.emai = emai;
+    }
+
+    public void setNumber(String n){
+        numbers.add(n);
+    }
+
+    public void showNumber(){
+        if(numbers.size() > 1){
+            System.out.print("Contact Number(s): ");
+        }
+        else {
+            System.out.print("Contact Number: ");
+        }
+        for(int i = 0;i < numbers.size();i++){
+            System.out.print(numbers.get(i));
+            if(i != numbers.size() - 1){
+                System.out.print(", ");
+            }
+        }
+    }
+
+    public void showPerson(){
+        System.out.println("-------- * -------- * -------- * --------");
+        System.out.println("First Name: "+first);
+        System.out.println("Last Name: "+last);
+        showNumber();
+        System.out.println("\n"+"Email address: "+emai);
+        System.out.println("-------- * -------- * -------- * --------");
+    }
+
+    @Override
+    public String toString(){
+        return first;
+    }
+
 }
