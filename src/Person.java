77959280a749643e288a83dad3d1cd361a01ab94
Arun verma <arun.verma_ccv18@gla.ac.in>
2020-02-24 22:16:07 +0530

    creating PERSON CLass

---
+++ src/Person.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Person {
+    private String first;
+    private String last;
+    private String temp;
+    private String emai;
+    private ArrayList<String> numbers = new ArrayList<>();
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+}
